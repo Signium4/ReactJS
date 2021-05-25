6ed922116f6a8e9092a5b12b35af90020da27466
Signium4 <signium4@gmail.com>
2021-05-25 23:35:21 +0300

    'hw5'

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,22 +1,29 @@
-import './Task.css';
+import styles from './Task.module.scss';
 import React, { Component } from 'react';
+import classnames from "classnames/bind"
+import { ThemeContext } from '../App/ThemeContext';
 
+const cx = classnames.bind(styles)
+
+// const theme = "dark"
 const Task = ({id, name, description, completed, changeStatus}) => {
     const handleClick = () => {
       changeStatus(id)
     };
     const status = completed? "Сделано": "Не сделано";
-    const par = "jnsdvkjnsd"
     return(
-    <span className="spanTask">
-      <div>
-        <span className='id'>{id}. </span>
-        <span className='name'>{name}</span>
-      </div>
-      <div className='description'>{description}</div>
-      <div className='completed'>{completed}</div>
-      <button className='button' onClick={handleClick}>{status}</button>
-    </span>
+    <ThemeContext.Consumer>
+      {(theme) =>
+      <span className={cx("spanTask", `spanTask-theme-${theme}`)}>
+        <div>
+          <span className={cx(`id-theme-${theme}`)}>{id}. </span>
+          <span className={cx(`name-theme-${theme}`)}>{name}</span>
+        </div>
+        <div className={cx(`description-theme-${theme}`)}>{description}</div>
+        <button className={cx(`button-theme-${theme}`,
+        `button-theme-${theme}-${completed? "done": "undone"}`)} onClick={handleClick}>{status}</button>
+      </span>}
+    </ThemeContext.Consumer>
     )
   }
 
